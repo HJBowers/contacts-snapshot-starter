9289540fd5640ad6c9b974fa5f9aed768c3f3c2b
leikkisa <sallymaki@gmail.com>
2017-09-06 09:50:12 -0700

    Add schema

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE contacts (
-  id serial,
+  id serial PRIMARY KEY,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
+);
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username varchar(255) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  admin boolean DEFAULT FALSE NOT NULL
 );
