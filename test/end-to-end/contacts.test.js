e949069382c3ffc16f736c26fb36a2eec489a031
SomayaB <somaya.bounouar@gmail.com>
2017-09-13 16:26:09 -0700

    GET / test working

--- test/end-to-end/contacts.test.js
+++ test/end-to-end/contacts.test.js
@@ -1,16 +1,31 @@
-const  chai = require('chai')
+const chai = require('chai')
 const expect = chai.expect
 const chaiHttp = require('chai-http')
 const app = require('../../src/server.js')
+const dbHelper = require('../helpers/db')
+const supertest = require('supertest')
+const sinon = require('sinon')
+const ejs = require('ejs')
+const path = require('path')
 
 chai.use(chaiHttp)
 
 describe('/', () => {
-  it('should return a 200 status code', () => {
-    return chai.request(app)
-      .get('/')
-      .then(res => {
-      expect(res).to.have.status(200)
+  beforeEach('truncate the DB', () => {
+    return dbHelper()
+  })
+
+  it('should return a 200 status code and checks that the correct page is rendered', () => {
+    var spy = sinon.spy(ejs, '__express')
+
+    return supertest(app)
+    .get('/')
+    .expect(200)
+    .then(res => {
+      //console.log(spy);
+      expect(spy.calledWith(path.resolve(__dirname + '/../../src/views/index.ejs'))).to.be.true
+      spy.restore()
     })
   })
+
 })
