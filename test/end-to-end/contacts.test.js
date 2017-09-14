4f67b70c66306331cd97e65bf56bccc43c355629
HJBowers <HJBowers@gmail.com>
2017-09-14 12:29:09 -0700

    tested POST for /contacts

--- test/end-to-end/contacts.test.js
+++ test/end-to-end/contacts.test.js
@@ -8,21 +8,19 @@ const sinon = require('sinon')
 const ejs = require('ejs')
 const path = require('path')
 
+const should = chai.should
 chai.use(chaiHttp)
 
 describe('/', () => {
   beforeEach('truncate the DB', () => {
     return dbHelper()
   })
-
   it('should return a 200 status code and checks that the index page is rendered', () => {
     var spy = sinon.spy(ejs, '__express')
-
     return supertest(app)
     .get('/')
     .expect(200)
     .then(res => {
-      //console.log(spy);
       expect(spy.calledWith(path.resolve(__dirname + '/../../src/views/index.ejs'))).to.be.true
       spy.restore()
     })
@@ -33,17 +31,33 @@ describe('/contacts/new', () => {
   beforeEach('truncate the DB', () => {
     return dbHelper()
   })
-
   it('should return a 200 status code and checks that the new page is rendered', () => {
     var spy = sinon.spy(ejs, '__express')
-
     return supertest(app)
     .get('/contacts/new')
     .expect(200)
     .then(res => {
-      //console.log(spy);
-      expect(spy.calledWith(path.resolve(__dirname + '/../../src/views/new.ejs'))).to.be.true
+      expect(spy.calledWith(path.resolve(__dirname + '/../.,/src/views/new.ejs'))).to.be.true
       spy.restore()
+    })
+  })
+})
+
+describe('/contacts', () => {
+  beforeEach('truncate the DB', () => {
+    return dbHelper()
+  })
+  it('should have a body with new contact, and database length should increase by 1', () => {
+    return supertest(app)
+    .post('/contacts/')
+    .send({
+      first_name: 'Murphy',
+      last_name: 'Cat'
+    })
+    .then( (res) => {
+      expect(res.headers.location).to.equal('/contacts/4')
+      expect(res).to.have.status(302)
+      expect(res).to.redirect
     })
   })
 })
