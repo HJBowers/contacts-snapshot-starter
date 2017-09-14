5547e27564e69d5f3e3a5433033b155b6ef66215
SomayaB <somaya.bounouar@gmail.com>
2017-09-14 15:28:20 -0700

    added tests for contactId, delete and search

--- test/end-to-end/contacts.test.js
+++ test/end-to-end/contacts.test.js
@@ -2,7 +2,7 @@ const chai = require('chai')
 const expect = chai.expect
 const chaiHttp = require('chai-http')
 const app = require('../../src/server.js')
-const dbHelper = require('../helpers/db')
+const dbTruncate = require('../helpers/db')
 const supertest = require('supertest')
 const sinon = require('sinon')
 const ejs = require('ejs')
@@ -13,7 +13,7 @@ chai.use(chaiHttp)
 
 describe('/', () => {
   beforeEach('truncate the DB', () => {
-    return dbHelper()
+    return dbTruncate()
   })
   it('should return a 200 status code and checks that the index page is rendered', () => {
     var spy = sinon.spy(ejs, '__express')
@@ -29,7 +29,7 @@ describe('/', () => {
 
 describe('/contacts/new', () => {
   beforeEach('truncate the DB', () => {
-    return dbHelper()
+    return dbTruncate()
   })
   it('should return a 200 status code and checks that the new page is rendered', () => {
     var spy = sinon.spy(ejs, '__express')
@@ -45,7 +45,7 @@ describe('/contacts/new', () => {
 
 describe('/contacts', () => {
   beforeEach('truncate the DB', () => {
-    return dbHelper()
+    return dbTruncate()
   })
   it('should have a body with new contact, and database length should increase by 1', () => {
     return supertest(app)
@@ -55,10 +55,58 @@ describe('/contacts', () => {
       last_name: 'Cat'
     })
     .then( (res) => {
-      console.log("TEST:::", res.headers);
       expect(res.headers.location).to.equal('/contacts/4')
       expect(res).to.have.status(302)
       expect(res).to.redirect
+    })
+  })
+})
+
+
+describe('/contacts/:contactId', () => {
+  beforeEach('truncate the DB', () => {
+    return dbTruncate()
+  })
+  it('should return a 200 status code and checks that the show page is rendered', () => {
+    var spy = sinon.spy(ejs, '__express')
+    return supertest(app)
+    .get('/contacts/1')
+    .expect(200)
+    .then(res => {
+      expect(spy.calledWith(path.resolve(__dirname + '/../../src/views/show.ejs'))).to.be.true
+      spy.restore()
+    })
+  })
+})
+
+
+describe('/contacts/:contactId/delete', () => {
+  beforeEach('truncate the DB', () => {
+    return dbTruncate()
+  })
+  it('should have a body with new contact, and database length should increase by 1', () => {
+    return supertest(app)
+    .delete('/contacts/1/delete')
+    return supertest(app)
+    .get('/contacts/1')
+    .then(res => {
+      expect(res.body).to.eql({})
+    })
+  })
+})
+
+describe.only('/search', () => {
+  beforeEach('truncate the DB', () => {
+    return dbTruncate()
+  })
+  it('should return a 200 status code and checks that the index page is rendered', () => {
+    var spy = sinon.spy(ejs, '__express')
+    return supertest(app)
+    .get('/contacts/search?q=j')
+    .expect(200)
+    .then(res => {
+      expect(spy.calledWith(path.resolve(__dirname + '/../../src/views/index.ejs'))).to.be.true
+      spy.restore()
     })
   })
 })
